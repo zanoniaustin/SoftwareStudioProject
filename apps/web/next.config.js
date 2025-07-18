7a08886286bb705b56640beb23ded5378d543992
da-87 <dan.arthur@drexel.edu>
2025-07-18 11:42:10 -0600

    Nx workspace

---
+++ apps/web/next.config.js
@@ -0,0 +1,34 @@
+//@ts-check
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { composePlugins, withNx } = require('@nx/next');
+
+/**
+ * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
+ **/
+const nextConfig = {
+  // Use this to set Nx-specific options
+  // See: https://nx.dev/recipes/next/next-config-setup
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.pokemontcg.io',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  nx: {
+    // Set this to true if you would like to use SVGR
+    // See: https://nx.dev/recipes/next/using-svgr
+    svgr: false,
+  },
+};
+
+const plugins = [
+  // Add more Next.js plugins to this list if needed.
+  withNx,
+];
+
+module.exports = composePlugins(...plugins)(nextConfig);
